4779cfe2c3203cb4003a2fa28d1352eae9aef22b
Tritiam <mutiaratritia06@gmail.com>
2021-11-01 12:40:17 +0700

    Update files

---
+++ app/src/main/java/com/example/healthylife/createFood.java
@@ -0,0 +1,14 @@
+package com.example.healthylife;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class createFood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_food);
+    }
+}
