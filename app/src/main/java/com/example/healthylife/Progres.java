00c079691a62e505df6f349bf7132a6cbecb6476
RafikaFadilaDaulay <rafikafadila15@gmail.com>
2021-11-01 19:04:25 +0700

    food calori, add food, remember water, progres

---
+++ app/src/main/java/com/example/healthylife/Progres.java
@@ -0,0 +1,14 @@
+package com.example.healthylife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Progres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progres);
+    }
+}
---
+++ app/src/main/java/com/example/healthylife/RememberWater.java
@@ -0,0 +1,14 @@
+package com.example.healthylife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RememberWater extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remember_water);
+    }
+}
