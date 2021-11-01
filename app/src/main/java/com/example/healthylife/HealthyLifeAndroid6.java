5b22a52ec1034f5aa6aac535f4bd5e2f7889fd5d
putrisiregarr <siregarrputri20@gmail.com>
2021-11-01 11:56:51 +0700

    healthylifee

---
+++ app/src/main/java/com/example/healthylife/HealthyLifeAndroid6.java
@@ -0,0 +1,14 @@
+package com.example.healthylife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HealthyLifeAndroid6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_healthy_life_android6);
+    }
+}
---
+++ app/src/main/java/com/example/healthylife/LogLunch.java
@@ -0,0 +1,14 @@
+package com.example.healthylife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogLunch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_lunch);
+    }
+}
